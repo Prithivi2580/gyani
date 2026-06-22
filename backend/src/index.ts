cff3266bf9e49d7ec9a1d9ef8310f7eae82e8d34
Prithivi <prithivi@users.noreply.github.com>
2026-06-22 20:49:21 +0545

    feat: initialize the prisma orm with neon db and better-auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,22 @@
+import "dotenv/config";
 import express, { type Express } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { auth } from "@/lib/auth";
+import { toNodeHandler } from "better-auth/node";
 
 const app: Express = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.use(cors({ origin: process.env.CORS_ORIGIN!, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ backend/src/lib/prisma-client.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
+}
+
+const adapter = new PrismaNeon({ connectionString });
+
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
